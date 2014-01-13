ff8c89e7b00111dcac1612d386d254cda14d24e0
tjadams <robocustom@hotmail.com>
2014-01-12 22:51:40 -0500

    Create point.java

---
+++ point.java
@@ -0,0 +1,22 @@
+// Tyler Adams - Friday June 7th, 2013
+
+public class point {
+	private double x;
+	private double y;
+	private double z;
+
+	public double returnx (){
+		return x;
+	}
+	public double returny (){
+		return y;
+	}
+	public double returnz (){
+		return z;
+	}
+	public point (double a, double b, double c){
+		x=a;
+		y=b;
+		z=c;
+	}
+}
